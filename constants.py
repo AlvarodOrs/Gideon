34765426217dd8acaaf997dafca2755e6b49b414
Álvaro d'Ors Nestares <75393390+AlvarodOrs@users.noreply.github.com>
2026-06-03 15:05:59 +0200

    Working

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-WEB_REPO = "TESTING" #"adors.dev"
+WEB_REPO = "adors.dev"
 WEB_REPO_PATH = "content/projects" #"adors.dev"
 EXCLUDED_REPOS = ["UNIR", "TESTING"]
 CATEGORIES = ['finance', 'engineering', 'utils', 'website']
